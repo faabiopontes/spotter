a791baf72e5fe6ad6744414fa98e8a3a1eecdfc8
Fabio Pontes <faabiopontes@gmail.com>
2021-04-28 14:08:56 -0300

    feat: Last Crash Points Saving to File

--- lastCrashPoints.js
+++ lastCrashPoints.js
@@ -1,7 +1,8 @@
 const crypto = require("crypto");
+const fs = require("fs");
 
 const lastServerSeed =
-  "81e0125d7039d605951b45f7a6e54ffbe59d831802937ae78ebbd39da222221b";
+  "78c8935bf1b4dbea848a60f0eb6da7c7a5c508d8a6610eb1f8ede8adea6cea3a";
 const amount = 3500;
 
 const chain = [lastServerSeed];
@@ -19,39 +20,48 @@ for (let i = 0; i < amount; i++) {
 const clientSeed =
   "0000000000000000000415ebb64b0d51ccee0bb55826e43846e5bea777d91966";
 
+const divisible = (hash, mod) => {
+  let val = 0;
+
+  let o = hash.length % 4;
+  for (let i = o > 0 ? o - 4 : 0; i < hash.length; i += 4) {
+    val = ((val << 16) + parseInt(hash.substring(i, i + 4), 16)) % mod;
+  }
+
+  return val === 0;
+};
+
+function getPoint(hash) {
+  // In 1 of 15 games the game crashes instantly.
+  if (divisible(hash, 15)) return 0;
+
+  // Use the most significant 52-bit from the hash to calculate the crash point
+  let h = parseInt(hash.slice(0, 52 / 4), 16);
+  let e = Math.pow(2, 52);
+
+  const point = (Math.floor((100 * e - h) / (e - h)) / 100).toFixed(2);
+
+  return point.replace(".", ",");
+}
+
 const crashPoints = chain.map((seed) => {
   const hash = crypto
     .createHmac("sha256", seed)
     .update(clientSeed)
     .digest("hex");
 
-  const divisible = (hash, mod) => {
-    let val = 0;
-
-    let o = hash.length % 4;
-    for (let i = o > 0 ? o - 4 : 0; i < hash.length; i += 4) {
-      val = ((val << 16) + parseInt(hash.substring(i, i + 4), 16)) % mod;
-    }
-
-    return val === 0;
-  };
-
-  function getPoint(hash) {
-    // In 1 of 15 games the game crashes instantly.
-    if (divisible(hash, 15)) return 0;
-
-    // Use the most significant 52-bit from the hash to calculate the crash point
-    let h = parseInt(hash.slice(0, 52 / 4), 16);
-    let e = Math.pow(2, 52);
-
-    const point = (Math.floor((100 * e - h) / (e - h)) / 100).toFixed(2);
-
-    return point;
-  }
-
   const point = getPoint(hash);
 
   return point;
 });
 
-console.log(crashPoints.join('\n'));
+const fileContent = crashPoints.reverse().join("\n");
+const fileName = "crash_points.csv";
+
+fs.writeFile(fileName, fileContent, (err) => {
+  if (err) {
+    return console.error(err);
+  }
+
+  console.log(`Last ${amount} Crash Points written to ${fileName}`);
+});
