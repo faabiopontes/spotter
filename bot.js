073c815e70451fd8d65b3e83af2b5307c26a7f39
Fabio Pontes <faabiopontes@gmail.com>
2021-05-09 14:08:05 -0300

    feat: sendMessage Groups

--- bot.js
+++ bot.js
@@ -2,14 +2,26 @@ const TelegramBot = require("node-telegram-bot-api");
 
 const token = process.env.TELEGRAM_TOKEN;
 const polling = eval(process.env.TELEGRAM_POLLING);
-const chatId = process.env.TELEGRAM_GROUP_ID;
+const groupSignalsId = process.env.TELEGRAM_GROUP_SIGNALS_ID;
+const groupChatId = process.env.TELEGRAM_GROUP_CHAT_ID;
 const adminChatId = process.env.TELEGRAM_ADMIN_ID;
 
 const bot = new TelegramBot(token, { polling: polling ? true : false });
 
 const sendMessage = (message) => {
   console.log(`bot.sendMessage: ${message}`);
-  bot.sendMessage(chatId, message, { parse_mode: 'HTML'});
+  bot.sendMessage(groupSignalsId, message, { parse_mode: 'HTML'});
+  bot.sendMessage(groupChatId, message, { parse_mode: 'HTML'});
+};
+
+const sendMessageGroupChat = (message) => {
+  console.log(`bot.sendMessageGroupChat: ${message}`);
+  bot.sendMessage(groupChatId, message, { parse_mode: 'HTML'});
+};
+
+const sendMessageGroupSignals = (message) => {
+  console.log(`bot.sendMessageGroupSignals: ${message}`);
+  bot.sendMessage(groupSignalsId, message, { parse_mode: 'HTML'});
 };
 
 const sendMessageAdmin = (message) => {
@@ -21,4 +33,4 @@ const sendMessageAdmin = (message) => {
 // bot.sendMessage(adminChatId, 'Quando vem um loss depois de 15 wins');
 // bot.sendVideoNote(adminChatId, './assets/videos/crying_money.mp4');
 
-module.exports = { sendMessage, sendMessageAdmin };
+module.exports = { sendMessage, sendMessageAdmin, sendMessageGroupChat, sendMessageGroupSignals };
